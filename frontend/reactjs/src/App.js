69faad0f59a23e05648690d3823d57dd7d3f88dd
Charith Wickramasinghe <pvmcharith@gmail.com>
2023-10-25 22:09:34 +0530

    v1.3

--- frontend/reactjs/src/App.js
+++ frontend/reactjs/src/App.js
@@ -1,11 +1,24 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-
+import{BrowserRouter,Routes,Route} from 'react-router-dom';
+import ShopCategory from './Pages/ShopCategory';
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+        <Route path="/product" element={<Product/>}/>
+        <Route path=':productId' element={<Product/>}/>
+      </Routes>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/login' element={<LoginSignup/>}/>
+      </BrowserRouter>
     </div>
   );
 }
