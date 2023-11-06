68817658bdf1b96509118c643d91e7ee8290e9f5
Charith Wickramasinghe <pvmcharith@gmail.com>
2023-11-06 16:22:02 +0530

    ver_1.9
    Single product details page completed

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ function menutoggle() {
     MenuItems.style.maxHeight = "0px";
   }
 }
+
+/*------------- js for product gallary -----------*/
+var ProductImg = document.getElementById("ProductImg");
+var SmallImg = document.getElementsByClassName("small-img");
+
+SmallImg[0].onclick = function () 
+{
+  ProductImg.src = SmallImg[0].src;
+}
+SmallImg[1].onclick = function () 
+{
+  ProductImg.src = SmallImg[1].src;
+}
+SmallImg[2].onclick = function () 
+{
+  ProductImg.src = SmallImg[2].src;
+}
+SmallImg[3].onclick = function () 
+{
+  ProductImg.src = SmallImg[3].src;
+}
