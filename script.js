12a97739edb93d4a5982bf0f1b25134f7dbbbaba
Charith Wickramasinghe <pvmcharith@gmail.com>
2023-11-10 23:40:05 +0530

    ver_1.13
    added login page

--- script.js
+++ script.js
@@ -10,6 +10,20 @@ function menutoggle() {
   }
 }
 /*------------- js for login form -----------*/
+var LoginForm = document.getElementById("LoginForm");
+var RegForm = document.getElementById("RegForm");
+var Indicator = document.getElementById("Indicator");
+
+function register() {
+  RegForm.style.transform = "translateX(0px)";
+  LoginForm.style.transform = "translateX(0px)";
+  Indicator.style.transform = "translateX(100px)";
+}
+function login() {
+  RegForm.style.transform = "translateX(300px)";
+  LoginForm.style.transform = "translateX(300px)";
+  Indicator.style.transform = "translateX(0px)";
+}
 
 /*------------- js for product gallary -----------*/
 var ProductImg = document.getElementById("ProductImg");
